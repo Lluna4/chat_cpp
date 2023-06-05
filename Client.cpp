b4075648cd7d2bf8c9f99dd883d7126afa05833c
Luna <69774917+carlyjb17@users.noreply.github.com>
2023-06-05 15:22:06 +0200

    Colour update

--- Client.cpp
+++ Client.cpp
@@ -10,10 +10,29 @@
 #include <commdlg.h>
 #include <Windows.h>
 #include <filesystem>
+#include <random>
 
 char* BUFFER = (char*)calloc(1024, sizeof(char));
 std::string USERNAME;
-std::vector<char*> MESSAGES;
+
+std::vector<ImVec4> COLORS = {
+	ImVec4(0.16f, 0.99f, 0.01f, 1.0f),  // Green
+	ImVec4(1.0f, 0.0f, 0.0f, 1.0f),     // Red
+	ImVec4(0.0f, 0.0f, 1.0f, 1.0f),     // Blue
+	ImVec4(1.0f, 1.0f, 0.0f, 1.0f),     // Yellow
+	ImVec4(1.0f, 0.0f, 1.0f, 1.0f),     // Magenta
+	ImVec4(0.0f, 1.0f, 1.0f, 1.0f),     // Cyan
+	ImVec4(0.5f, 0.2f, 0.8f, 1.0f),     // Lavender
+	ImVec4(1.0f, 0.5f, 0.0f, 1.0f),     // Orange
+	ImVec4(0.0f, 1.0f, 0.0f, 1.0f),     // Lime
+	ImVec4(0.5f, 0.0f, 0.5f, 1.0f),     // Purple
+	ImVec4(0.0f, 0.5f, 0.5f, 1.0f),     // Teal
+	ImVec4(0.0f, 0.5f, 1.0f, 1.0f),     // Sky Blue
+	ImVec4(0.5f, 0.0f, 0.0f, 1.0f),     // Maroon
+	ImVec4(0.0f, 0.0f, 0.5f, 1.0f),     // Navy
+	ImVec4(0.5f, 0.5f, 0.0f, 1.0f),     // Olive
+	ImVec4(0.5f, 0.0f, 0.5f, 1.0f)      // Indigo
+};
 SOCKET client_socket;
 const std::string SERVER_IP = "127.0.0.1";
 const int SERVER_PORT = 5050;
@@ -57,7 +76,19 @@ std::string open_dialog()
 	return returnValue;
 }
 
+ImVec4 getRandomElement(const std::vector<ImVec4>& vec) {
+	std::random_device rd; // Obtain a random number from hardware
+	std::mt19937 eng(rd()); // Seed the generator
 
+	// Define the range of indices
+	std::uniform_int_distribution<> distr(0, vec.size() - 1);
+
+	// Generate a random index
+	int randomIndex = distr(eng);
+
+	// Return the random element
+	return vec[randomIndex];
+}
 
 char* get_time()
 {
@@ -72,53 +103,61 @@ char* get_time()
 	return time_str;
 }
 
-void receive(SOCKET client)
+class User
 {
-	while (true)
-	{
-		if (FIRST_RECV == true)
-		{
-			recv(client, KEY, 1030, 0);
-			FIRST_RECV = false;
-			continue;
-		}
-		char* msg = (char*)calloc(1024, sizeof(char));
-		char* time = (char*)calloc(6, sizeof(char));
-		char* full_img = (char*)calloc(1025, sizeof(char));
-		recv(client, msg, 1024, 0);
-		if (strcmp(decode_text(_strdup(msg), KEY), "/image") == 0)
-		{
-			size_t datasize;
-			if (!std::filesystem::exists("resources/"))
-			{
-				std::filesystem::create_directories("./resources");
-			}
-			std::ofstream ofs(std::format("/resources/{}", ft_itoa(FILE_NAME)).c_str());
-			ofs.close();
-			FILE* fd = fopen(std::format("/resources/{}", ft_itoa(FILE_NAME)).c_str(), "wb");
-			char* text = (char*)calloc(1024, sizeof(char));
-			bool received = false;
-			while (!received)
-			{
-				datasize = recv(client_socket, text, 1024, 0);
-				fwrite(text, 1, datasize, fd);
+	public:
+		User() {}
 
-				received = datasize == 0;
-			}
-			fclose(fd);
-			FILE_NAME++;
-		}
-		if (msg[0] != ' ' && msg[0] != NULL)
-			MESSAGES.push_back(decode_text(_strdup(msg), KEY));
-		if (full_img)
+		User(char *username) 
+			:username_(username)
+		{}
+
+		char* get_username()
 		{
-			MESSAGES.push_back(_strdup(full_img));
+			return username_;
 		}
-		free(msg);
-		free(time);
-		free(full_img);
-	}
-}
+
+		ImVec4 get_color()
+		{
+			return color;
+		}
+
+	private:
+		char* username_;
+		ImVec4 color = getRandomElement(COLORS); 
+};
+
+class Message
+{
+	public:
+		Message() {}
+
+		Message(char *msg, User usuario)
+			:msg_(msg), usuario_(usuario)
+		{}
+
+		char* get_message()
+		{
+			return msg_;
+		}
+
+		char* get_username()
+		{
+			return usuario_.get_username();
+		}
+
+		ImVec4 get_color()
+		{
+			return usuario_.get_color();
+		}
+
+	private:
+		char* msg_;
+		User usuario_;
+
+};
+std::vector<Message> MESSAGES;
+std::vector<User> USERS;
 
 char* get_username(char* msg)
 {
@@ -131,6 +170,59 @@ char* get_username(char* msg)
 }
 
 
+void receive(SOCKET client)
+{
+	while (true)
+	{
+		if (FIRST_RECV == true)
+		{
+			recv(client, KEY, 1030, 0);
+			FIRST_RECV = false;
+			continue;
+		}
+		char* msg = (char*)calloc(1024, sizeof(char));
+		char* time = (char*)calloc(6, sizeof(char));
+		recv(client, msg, 1024, 0);
+		if (msg[0] != ' ' && msg[0] != NULL)
+		{
+			msg = decode_text(_strdup(msg), KEY);
+			if (strstr(msg, ":") != NULL)
+			{
+				if (USERS.size() > 0)
+				{ 
+					bool found = false;
+					for (unsigned int i = 0; i < USERS.size(); i++)
+					{
+						if (strcmp(USERS[i].get_username(), get_username(msg)) == 0)
+						{
+							MESSAGES.push_back(Message(_strdup(msg), USERS[i]));
+							found = true;
+							break;
+						}
+					}
+					if (found == false)
+					{
+						User usr = User(get_username(msg));
+						MESSAGES.push_back(Message(_strdup(msg), usr));
+						USERS.push_back(usr);
+					}
+				}
+				else
+				{
+					User usr = User(get_username(msg));
+					MESSAGES.push_back(Message(_strdup(msg), usr));
+					USERS.push_back(usr);
+				}
+				
+				continue ;
+			}
+			MESSAGES.push_back(Message(_strdup(msg), User()));
+		}
+		free(msg);
+		free(time);
+	}
+}
+
 class ExampleLayer : public Walnut::Layer
 {
 public:
@@ -140,38 +232,42 @@ public:
 		ImGui::Begin("Log");
 
 		if (MESSAGES.size() != 0)
+		{
 			for (unsigned int i = 0; i < MESSAGES.size(); i++)
 			{
-				if (strstr(MESSAGES[i], ":") != NULL)
+				if (strstr(MESSAGES[i].get_message(), ":") != NULL)
 				{
-					if (strcmp(get_username(MESSAGES[i - 1]), get_username(MESSAGES[i])) != 0)
+					if (i > 0)
 					{
 						ImGui::Text(" ");
-						ImGui::Text(get_username(MESSAGES[i]));
+						ImGui::TextColored(MESSAGES[i].get_color(), MESSAGES[i].get_username());
 						ImGui::SameLine();
 						ImGui::SetWindowFontScale(0.8f);
 						ImGui::TextColored(ImVec4(0.38f, 0.4f, 0.42f, 0.9f), _strdup(get_time()));
 						ImGui::SetWindowFontScale(1.0f);
 					}
-					ImGui::Text(strstr(MESSAGES[i], " "));
-				}
-				else if (strcmp(get_username(MESSAGES[i]), "/images") == 0)
-				{
-					std::make_shared<Walnut::Image>(250, 250, Walnut::ImageFormat::RGBA, MESSAGES[i + 1]);
-					ImGui::Image(m_image->GetDescriptorSet(), { (float)m_image->GetWidth(), (float)m_image->GetHeight() });
+					ImGui::Text(strstr(MESSAGES[i].get_message(), " "));
 				}
 				else
 				{
-					ImGui::Text(MESSAGES[i]);
+					if (strstr(MESSAGES[i].get_message(), "se conect"))
+					{
+						ImGui::TextColored(ImVec4(0.16f, 0.99f, 0.01f, 1.0f), MESSAGES[i].get_message());
+					}
+					else
+					{
+						ImGui::Text(MESSAGES[i].get_message());
+					}
 					//m_image = std::make_shared<Walnut::Image>("test.png");
 					//ImGui::Image(m_image->GetDescriptorSet(), {(float)m_image->GetWidth(), (float)m_image->GetHeight()});
 				}
 
 			}
+		}
 		ImGui::End();
 	}
-	private:
-		std::shared_ptr<Walnut::Image> m_image;
+	/*private:
+		std::shared_ptr<Walnut::Image> m_image;*/
 };
 
 class ChatLayer : public Walnut::Layer
@@ -208,7 +304,7 @@ public:
 			// enter was pressed 
 			if (strcmp(BUFFER, "/fps") == 0)
 			{
-				MESSAGES.push_back(_strdup(ft_itoa((int)ImGui::GetIO().Framerate)));
+				MESSAGES.push_back(Message(_strdup(ft_itoa((int)ImGui::GetIO().Framerate)), User()));
 			}
 			else
 			{
@@ -243,7 +339,7 @@ Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 {
     WSADATA wsa;
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        MESSAGES.push_back(_strdup("WSAStartup failed: "));
+        std::cerr << "WSAStartup failed: ";
     }
 	client_socket = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server_address;
@@ -253,8 +349,7 @@ Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 
     if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == SOCKET_ERROR)
     {
-		MESSAGES.push_back(_strdup("Connect failed: "));
-		MESSAGES.push_back(_strdup(ft_itoa(WSAGetLastError())));
+		std::cerr << "Connect failed: " << WSAGetLastError();
         closesocket(client_socket);
         WSACleanup();
     }
@@ -284,14 +379,14 @@ Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 					std::ofstream save("sav");
 					save << USERNAME;
 					save.close();
-					MESSAGES.push_back(_strdup("Se ha guardado el nombre de usuario"));
+					MESSAGES.push_back(Message(_strdup("Se ha guardado el nombre de usuario"), User()));
 				}
 				if (ImGui::MenuItem("Eliminar usuario"))
 				{
 					if (std::filesystem::exists("sav") == true)
 					{
 						remove("sav");
-						MESSAGES.push_back(_strdup("Se ha borrado el nombre de usuario guardado"));
+						MESSAGES.push_back(Message(_strdup("Se ha borrado el nombre de usuario guardado"), User()));
 					}
 				}
 				ImGui::EndMenu();
